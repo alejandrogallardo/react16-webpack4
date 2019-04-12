2946db0269d6aec66b6a8f37332218b677c2c0f0
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2019-04-12 17:18:07 -0600

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+    render(){
+        return (
+            <div className="container">
+                <h1>React App with Webpack 4</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
